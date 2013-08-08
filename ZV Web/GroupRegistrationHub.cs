fc1fc0a0e7da5e7772ff059de28cc1b1b82627a5
donQwedro <andrew_qwer@tut.by>
2013-08-08 17:37:13 +0300

    added EF 5.0 and StructureMap

--- ZV Web/GroupRegistrationHub.cs
+++ ZV Web/GroupRegistrationHub.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace ZV_Web
 {
+    [HubName("groupRegistration")]
     public class GroupRegistrationHub : Hub
     {
         public void Hello(string user)
         {
-            Clients.All.hello(user);
+            Clients.Others.hello(user);
         }
     }
 }
